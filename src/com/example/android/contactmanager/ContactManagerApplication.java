e9012a056acb658473499a3ebc6a246c9e3b16a1
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2013-11-03 03:05:50 -0500

    New icon and login on app launch

--- src/com/example/android/contactmanager/ContactManagerApplication.java
+++ src/com/example/android/contactmanager/ContactManagerApplication.java
@@ -1,9 +1,15 @@
 package com.example.android.contactmanager;
 
 import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyUserCallback;
+import com.kinvey.java.User;
+
 import android.app.Application;
+import android.util.Log;
 
 public class ContactManagerApplication extends Application {
+	
+	public static final String TAG = "ContactManagerApplication";
 
 	private Client mKinveyClient;
 	
@@ -15,6 +21,17 @@ public class ContactManagerApplication extends Application {
 	     * Creates kinvey client for communication with backend.
 	     */
 		mKinveyClient = new Client.Builder(this.getApplicationContext()).build();
+		
+		mKinveyClient.user().login(new KinveyUserCallback() {
+		    @Override
+		    public void onFailure(Throwable error) {
+		        Log.e(TAG, "Login Failure", error);
+		    }
+		    @Override
+		    public void onSuccess(User result) {
+		        Log.i(TAG,"Logged in a new implicit user with id: " + result.getId());
+		    }
+		});
 	}
 	
 	public void setsClient(Client myClient) {
